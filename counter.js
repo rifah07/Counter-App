d7b3353a39f7275752511e6c109226efee032ffa
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-04-30 21:54:26 +0600

    final change

--- counter.js
+++ counter.js
@@ -1,42 +1,31 @@
-let count=0;
-const displayElement=document.getElementById('counter');
-const incrementButton=document.getElementById('increment');
-incrementButton.addEventListener('click' , incrementCount);
-
-const decrementButton=document.getElementById('decrement');
-decrementButton.addEventListener('click' , decrementCount);
+let count = 0;
+const displayElement = document.getElementById("counter");
+const incrementButton = document.getElementById("increment");
+const decrementButton = document.getElementById("decrement");
 
 
 
-
-function incrementCount(){
-  if(count>=10 )
-{
-    alert('You have reached to the maximum value');
-    displayElement.innerText=count;
-  return count;
-
-}
-   else{
+function incrementCount() {
+  if (count >= 10) {
+    alert("You have reached to the maximum value");
+    displayElement.innerText = count;
+    return ;
+  } 
     count++;
-    displayElement.innerText=count;}
-   
+    displayElement.innerText = count;
+  
 }
-
-
-
 
 function decrementCount() {
-
-  if(count<=-10 )
-{
-    alert('You have reached to the minimum value');
-    displayElement.innerText=count;
-    return count;
-
+  if (count <= 0) {
+    alert("You have reached to the minimum value");
+    displayElement.innerText = count;
+    return;
+  } 
+    count--;
+    displayElement.innerText = count;
+  
 }
 
-else{
-  count --;
-  displayElement.innerText = count;}
-}
+incrementButton.addEventListener("click", incrementCount);
+decrementButton.addEventListener("click", decrementCount);
